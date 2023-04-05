5c7fd343e77a469786b808a4ea00eb21056e2c6e
Anish-CRYPTO <56535757+Anish-CRYPTO@users.noreply.github.com>
2023-04-05 12:54:38 +0200

    busy

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,20 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Genre;
+use App\Form\GenreType;
 use App\Repository\GenreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/genre', name: 'app_home')]
+    #[Route('/genre', name: 'insert')]
     public function genre(GenreRepository $genreRepository): Response
     {
         $genres = $genreRepository->findAll();
 
         return $this->render('home/index.html.twig', [
-            'Genre' => $genres,
+            'Genres' => $genres,
+        ]);
+    }
+
+    #[Route('/add/genre', name: 'add')]
+    public function submit(Request $request) : Response
+    {
+        $genre = new genre();
+        $form = $this->createForm(GenreType::class, $genre);
+
+        return $this->renderForm('home/add.html.twig',[
+            'genre' => $form,
         ]);
     }
 }
